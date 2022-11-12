b69d771a49523ed1668f34abfd9770101bbba170
saurabh <mahtosaurabh1@gmail.com>
2022-11-13 00:06:13 +0530

    navbar completed

--- E-Dashboard/backend/db/config.js
+++ E-Dashboard/backend/db/config.js
@@ -1,3 +1,3 @@
 const mongoose=require('mongoose')
-
+const url="mongodb+srv://dbUser:<password>@cluster0.qhzt788.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect('mongodb://localhost:27017/e-commerce');
